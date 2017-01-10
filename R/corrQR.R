49958d8070eb406be184fde15f6ae4081c10ada0
Dave Klemish <dklemish@outlook.com>
2017-01-10 13:01:53 -0500

    Updated R code to use qrjoint for initialization

--- R/corrQR.R
+++ R/corrQR.R
@@ -37,7 +37,7 @@ corrQR <- function(x, y, nsamp = 1e3, thin = 10,
   # blocks.s -
   # fix.nu - option on whether nu parameter should be included in MCMC
 
-  ## Read in data
+  #### Read in data ####
   x <- as.matrix(x)
   n <- nrow(x)
   p <- ncol(x)
@@ -57,30 +57,7 @@ corrQR <- function(x, y, nsamp = 1e3, thin = 10,
   # Map selected copula to numeric code
   # if(copula == "gaussian") copulaMethod <- 1
 
-  # Determine the grid of quantiles of interest as multiples of 0.01
-  # plus additional points in the tail based on the # of data points
-
-  Ltail <- ceiling(log(n*incr,2)) # of points in tail
-
-  # tau.g = grid of quantiles
-  # L = # of quantiles
-  # mid = array position of quantile closest to 0.5
-  # reg.ix = indices of quantiles not in the tail
-  if(Ltail > 0){
-    tau.tail <- incr / 2^(Ltail:1)
-    tau.g <- c(0, tau.tail, seq(incr, 1 - incr, incr), 1 - tau.tail[Ltail:1], 1)
-    L <- length(tau.g)
-    mid <- which.min(abs(tau.g - 0.5))
-    reg.ix <- (1:L)[-c(1 + 1:Ltail, L - 1:Ltail)]
-  } else {
-    tau.g <- seq(0, 1, incr)
-    L <- length(tau.g)
-    mid <- which.min(abs(tau.g - 0.5))
-    reg.ix <- (1:L)
-  }
-
-  # tau.k = reduced grid of quantiles at which functions will be tracked
-  tau.k <- seq(0,1,len = nknots)
+  #### Necessary hyperparameters ####
 
   a.sig <- hyper$sig;
   if(is.null(a.sig))
@@ -100,11 +77,49 @@ corrQR <- function(x, y, nsamp = 1e3, thin = 10,
 
   hyperPar <- c(a.sig, c(a.kap))
 
-  # prox.grid = points approximating possible values of lambda
-  #  (scale parameter in GPs)
-  # prior.grid = discrete approximation to value of prior pdf for
-  #  possible values of lambda as stored in prox.grid
-  # lp.grid = approximate log of prior distribution for lambda
+
+  #### Set up quantile grid ####
+
+  # Grid of quantiles of interest includes multiples of "incr"
+  # plus additional points in the tail based on the # of data points
+
+  Ltail <- ceiling(log(n*incr,2)) # of points in tail
+
+  # tau.g = grid of quantiles
+  # L = # of quantiles
+  # mid = array position of quantile closest to 0.5
+  # reg.ix = indices of quantiles not in the tail
+  # tau.k = reduced grid of quantiles at which functions will be tracked
+
+  if(Ltail > 0){
+    tau.tail <- incr / 2^(Ltail:1)
+    tau.g <- c(0, tau.tail, seq(incr, 1 - incr, incr), 1 - tau.tail[Ltail:1], 1)
+    L <- length(tau.g)
+    mid <- which.min(abs(tau.g - 0.5))
+    reg.ix <- (1:L)[-c(1 + 1:Ltail, L - 1:Ltail)]
+  } else {
+    tau.g <- seq(0, 1, incr)
+    L <- length(tau.g)
+    mid <- which.min(abs(tau.g - 0.5))
+    reg.ix <- (1:L)
+  }
+
+  tau.k <- seq(0,1,len = nknots)
+
+  #### Discrete approximation to prior on lambda ####
+  # See section 3.2 of Yang & Tokdar
+
+  # prox.grid  = points approximating possible values of lambda (scale parameter in GPs)
+  # prior.grid = approximation to prior pdf for lambda at values of prox.grid
+  # lp.grid    = log of prior.grid
+  # d.kg       = (tau - tau')^expo for tau \in tau.k, tau' \in tau.g
+  # d.kk       = (tau - tau')^expo for tau \in tau.k, tau' \in tau.k
+  #  d.kg, d.kk used for computation of A_g (each g corresponds to different
+  #  possible value of lambda)
+  # gridmats   = GP parameters to pass to MCMC
+  #  ith column contains A_g, R_g, the log determinant of R_g, and the
+  #  log prior density for that value of lambda
+
   prox.grid  <- proxFn(max(prox.range), min(prox.range), 0.5)
   ngrid      <- length(prox.grid)
   lamsq.grid <- lamFn(prox.grid)^2
@@ -115,19 +130,12 @@ corrQR <- function(x, y, nsamp = 1e3, thin = 10,
                             a.lam[2]))
   lp.grid <- log(prior.grid)
 
-  # d.kg = (tau_i - tau'_j)^2 for tau_i \in tau.k (reduced quantile grid),
-  #   tau_j \in tau.g (full quantile grid)
   d.kg <- abs(outer(tau.k, tau.g, "-"))^expo
-
-  # d.kk = (tau_i - tau'_j)^2 for tau_i \in tau.k (reduced quantile grid),
-  #   tau_j \in tau.k
   d.kk <- abs(outer(tau.k, tau.k, "-"))^expo
 
-  # gridmats = GP parameters to pass to MCMC
-  #  ith column contains A_g, R_g, the log determinant of R_g, and the
-  #  log prior density for that value of lambda
   gridmats <- matrix(NA, nknots*(L + nknots)+2, ngrid)
 
+  # Initialize gridmats
   K0 <- 0
   t1 <- Sys.time()
   for(i in 1:ngrid){
@@ -148,15 +156,18 @@ corrQR <- function(x, y, nsamp = 1e3, thin = 10,
   t2 <- Sys.time()
   cat("Matrix calculation time per 1e3 iterations =", round(1e3 * as.numeric(t2 - t1), 2), "\n")
 
-  niter <- nsamp * thin
-  dimpars <- c(n, p, q, L, mid - 1, nknots, ngrid, ncol(a.kap), niter, thin, nsamp)
-
-  # Number of parameters for each response variable
+  # npar  = number of parameters for each response variable
+  # niter = number of MCMC iterations
+  # ncorr = number of correlation parameters for Gaussian copula
   npar <- (nknots+1) * (p+1) + 2
+  niter  <- nsamp * thin
+  n.corr <- q*(q-1)/2
 
-  # Measure of influence used to better initialize sigma before
-  # starting MCMC
-  infl <- rep(0, q)
+  dimpars <- c(n, p, q, L, mid - 1, nknots, ngrid, ncol(a.kap), niter, thin, nsamp, n.corr)
+
+  #### Initialize parameter vector for better MCMC mixing
+  # Use qrjoint on individual responses as starting estimate
+  # for parameters of each response
 
   # par vector contains paramters to track in MCMC
   #  First response
@@ -179,113 +190,17 @@ corrQR <- function(x, y, nsamp = 1e3, thin = 10,
   #    (q-1)*npar + (p+1)*(nknots+1) + 1 = sigma_q
   #    (q-1)*npar + (p+1)*(nknots+1) + 2 = nu_q
   #  Copula parameters
-  #    q*npar_j + 1 = 1 parameter copula parameter
+  #    q*npar + 1:ncorr = copula parameters
 
-  if(initPar[1] == "prior") {
-    par <- rep(0, q*npar + q*(q-1)/2)
-    if(fix.nu) par[npar * 1:q] <- nuFn.inv(fix.nu)
+  par <- rep(0, q*npar + n.corr)
 
-    for(j in 1:q){
-      # Initialize gamma parameters
-      beta.rq <- sapply(tau.g, function(a) return(coef(rq(y[,j] ~ x, tau = a))))
-      v <- bs(tau.g, df = 5)
-      rq.lm <- apply(beta.rq, 1, function(z) return(coef(lm(z ~ v))))
-
-      delta <- tau.g[2]
-      tau.0 <- tau.g[mid]
-      rq.tau0 <- c(c(1, predict(v, tau.0)) %*% rq.lm)
-      rq.delta <- c(c(1, predict(v, delta)) %*% rq.lm)
-      rq.deltac <- c(c(1, predict(v, 1 - delta)) %*% rq.lm)
-
-      par[(j-1)*npar + nknots*(p+1) + 1:(p+1)] <- as.numeric(rq.tau0)
-
-      # Initialize transformed value of sigma
-      sigma <- 1
-      par[(j-1)*npar + (nknots+1)*(p+1) + 1] <- sigFn.inv(sigma, a.sig)
-
-      # Initialize values of w_0 to w_p functions
-      for(i in 1:(p+1)){
-        kapsq <- sum(exp(a.kap[3,]) * (a.kap[2,] / a.kap[1,]))
-        lam.ix <- sample(length(lamsq.grid), 1, prob = prior.grid)
-        R <- matrix(gridmats[L*nknots + 1:(nknots*nknots),lam.ix], nknots, nknots)
-        z <- sqrt(kapsq) * c(crossprod(R, rnorm(nknots)))
-        par[(j-1)*npar + (i - 1) * nknots + 1:nknots] <- z - mean(z)
-      }
-
-      beta.hat <- estFn(par, x, y[,j], gridmats, L, mid, nknots, ngrid, a.kap, a.sig, tau.g, reg.ix, FALSE)
-      qhat <- tcrossprod(cbind(1, x), beta.hat)
-
-      infl[j] <- max(max((y[,j] - qhat[,mid])/(qhat[,ncol(qhat) - 1] - qhat[,mid])),
-                     max((qhat[,mid] - y[,j])/(qhat[,mid] - qhat[,2])))
-    }
-  } else if (initPar[1] == "RQ"){
-
-    # par contains MCMC parameters to track
-    #  - npar paramters for each response
-    #  - q*(q-1)/2 correlation parameters for Gaussian copula
-    par <- rep(0, q*npar + q*(q-1)/2)
-
-    for(j in 1:q){
-      # Initialize gamma parameters
-      beta.rq <- sapply(tau.g, function(a) return(coef(rq(y[,j] ~ x, tau = a))))
-      v <- bs(tau.g, df = 5)
-      rq.lm <- apply(beta.rq, 1, function(z) return(coef(lm(z ~ v))))
-
-      delta <- tau.g[2]
-      tau.0 <- tau.g[mid]
-      rq.tau0 <- c(c(1, predict(v, tau.0)) %*% rq.lm)
-      rq.delta <- c(c(1, predict(v, delta)) %*% rq.lm)
-      rq.deltac <- c(c(1, predict(v, 1 - delta)) %*% rq.lm)
-
-      par[(j-1)*npar + nknots*(p+1) + 1:(p+1)] <- as.numeric(rq.tau0)
-
-      # Initialize sigma
-      nu <- ifelse(fix.nu, fix.nu, nuFn(0))
-
-      sigma <- min((rq.delta[1] - rq.tau0[1]) / Q0(delta, nu), (rq.deltac[1] - rq.tau0[1]) / Q0(1 - delta, nu))
-      par[(j-1)*npar + (nknots+1)*(p+1) + 1]  <- sigFn.inv(sigma, a.sig)
-
-      epsilon <- 0.1 * min(diff(sort(tau.k)))
-      tau.knot.plus <- pmin(tau.k + epsilon, 1)
-      tau.knot.minus <- pmax(tau.k - epsilon, 0)
-      beta.rq.plus <- cbind(1, predict(v, tau.knot.plus)) %*% rq.lm
-      beta.rq.minus <- cbind(1, predict(v, tau.knot.minus)) %*% rq.lm
-      zeta0.plus <- F0((beta.rq.plus[,1] - rq.tau0[1]) / sigma, nu)
-      zeta0.minus <- F0((beta.rq.minus[,1] - rq.tau0[1]) / sigma, nu)
-      zeta0.dot.knot <- (zeta0.plus - zeta0.minus) / (tau.knot.plus - tau.knot.minus)
-      w0.knot <- log(pmax(epsilon, zeta0.dot.knot)) / shrinkFn(p)
-      w0.knot <- (w0.knot - mean(w0.knot))
-
-      w0PP <- ppFn0(w0.knot, gridmats, L, nknots, ngrid)
-      w0 <- w0PP$w
-
-      zeta0.dot <- exp(shrinkFn(p) * (w0 - max(w0)))
-      zeta0 <- trape(zeta0.dot[-c(1,L)], tau.g[-c(1,L)], L-2)
-      zeta0.tot <- zeta0[L-2]
-      zeta0 <- c(0, tau.g[2] + (tau.g[L-1]-tau.g[2])*zeta0 / zeta0.tot, 1)
-      zeta0.dot <- (tau.g[L-1]-tau.g[2])*zeta0.dot / zeta0.tot
-      zeta0.dot[c(1,L)] <- 0
-
-      # Initialize w0_j
-      par[(j-1)*npar_j + 1:nknots] <- w0.knot
-
-      beta0.dot <- sigma * q0(zeta0, nu) * zeta0.dot
-
-      tilt.knot <- tau.g[tilt.ix <- sapply(tau.k, function(a) which.min(abs(a - zeta0)))]
-      tau.knot.plus  <- pmin(tilt.knot + epsilon, 1)
-      tau.knot.minus <- pmax(tilt.knot - epsilon, 0)
-      beta.rq.plus  <- cbind(1, predict(v, tau.knot.plus)) %*% rq.lm
-      beta.rq.minus <- cbind(1, predict(v, tau.knot.minus)) %*% rq.lm
-
-      beta.dot.knot <- (beta.rq.plus[,-1,drop=FALSE] - beta.rq.minus[,-1,drop=FALSE]) /  (tau.knot.plus - tau.knot.minus)
-
-      # Initialize w(1:p)_j
-      par[(j-1)*npar + nknots + 1:(nknots*p)] <- c(beta.dot.knot)
-      beta.hat <- estFn(par, x, y[,j], gridmats, L, mid, nknots, ngrid, a.kap, a.sig, tau.g, reg.ix, FALSE)
-      qhat <- tcrossprod(cbind(1, x), beta.hat)
-      infl[j] <- max(max((y[,j] - qhat[,mid])/(qhat[,ncol(qhat) - 1] - qhat[,mid])),
-                     max((qhat[,mid] - y[,j])/(qhat[,mid] - qhat[,2])))
-    }
+  for(j in 1:q){
+    suppressWarnings(
+      invis <- capture.output(
+        par[(j-1)*npar + 1:npar] <-
+          qrjoint(x, y[,j], nsamp = 100, thin=5)$par
+      )
+    )
   }
 
   # Build list that contains which indices of parameter vector
@@ -490,54 +405,6 @@ corrQR <- function(x, y, nsamp = 1e3, thin = 10,
   return(oo)
 }
 
-estFn <- function(par, x, y, gridmats, L, mid, nknots, ngrid, a.kap, a.sig, tau.g, reg.ix, reduce = TRUE, x.ce = 0, x.sc = 1){
-
-  n <- length(y)
-  p <- ncol(x)
-
-  wKnot <- matrix(par[1:(nknots*(p+1))], nrow = nknots)
-  w0PP  <- ppFn0(wKnot[,1], gridmats, L, nknots, ngrid)
-  w0    <- w0PP$w
-
-  wPP   <- apply(wKnot[,-1,drop=FALSE], 2, ppFn, gridmats = gridmats, L = L, nknots = nknots, ngrid = ngrid, a.kap = a.kap)
-
-  vMat <- matrix(sapply(wPP, extract, vn = "w"), ncol = p)
-
-  zeta0.dot <- exp(shrinkFn(p) * (w0 - max(w0)))
-  zeta0 <- trape(zeta0.dot[-c(1,L)], tau.g[-c(1,L)], L-2)
-  zeta0.tot <- zeta0[L-2]
-  zeta0 <- c(0, tau.g[2] + (tau.g[L-1]-tau.g[2])*zeta0 / zeta0.tot, 1)
-  zeta0.dot <- (tau.g[L-1]-tau.g[2])*zeta0.dot / zeta0.tot
-  zeta0.dot[c(1,L)] <- 0
-
-  reach <- nknots*(p+1)
-  gam0 <- par[reach + 1]; reach <- reach + 1
-  gam <- par[reach + 1:p]; reach <- reach + p
-  sigma <- sigFn(par[reach + 1], a.sig); reach <- reach + 1
-  nu <- nuFn(par[reach + 1]);
-
-  b0dot <- sigma * q0(zeta0, nu) * zeta0.dot
-  beta0.hat <- rep(NA, L)
-  beta0.hat[mid:L] <- gam0 + trape(b0dot[mid:L], tau.g[mid:L], L - mid + 1)
-  beta0.hat[mid:1] <- gam0 + trape(b0dot[mid:1], tau.g[mid:1], mid)
-
-  vNorm <- sqrt(rowSums(vMat^2))
-  a <- tcrossprod(vMat, x)
-  aX <- apply(-a, 1, max)/vNorm
-  aX[is.nan(aX)] <- Inf
-  aTilde <- vMat / (aX * sqrt(1 + vNorm^2))
-  ab0 <- b0dot * aTilde
-
-  beta.hat <- kronecker(rep(1,L), t(gam))
-  beta.hat[mid:L,] <- beta.hat[mid:L,] + apply(ab0[mid:L,,drop=FALSE], 2, trape, h = tau.g[mid:L], len = L - mid + 1)
-  beta.hat[mid:1,] <- beta.hat[mid:1,] + apply(ab0[mid:1,,drop=FALSE], 2, trape, h = tau.g[mid:1], len = mid)
-  beta.hat <- beta.hat / x.sc
-  beta0.hat <- beta0.hat - rowSums(beta.hat * x.ce)
-  betas <- cbind(beta0.hat, beta.hat)
-  if(reduce) betas <- betas[reg.ix,,drop = FALSE]
-  return(betas)
-}
-
 waic <- function(logliks, print = TRUE){
   lppd <- sum(apply(logliks, 1, logmean))
   p.waic.1 <- 2 * lppd - 2 * sum(apply(logliks, 1, mean))
@@ -548,56 +415,14 @@ waic <- function(logliks, print = TRUE){
   invisible(c(WAIC1 = waic.1, WAIC2 = waic.2))
 }
 
-ppFn0 <- function(w.knot, gridmats, L, nknots, ngrid){
-  w.grid <- matrix(NA, L, ngrid)
-  lpost.grid <- rep(NA, ngrid)
-  for(i in 1:ngrid){
-    A <- matrix(gridmats[1:(L*nknots),i], nrow = nknots)
-    R <- matrix(gridmats[L*nknots + 1:(nknots*nknots),i], nrow = nknots)
-    r <- sum.sq(backsolve(R, w.knot, transpose = TRUE))
-    w.grid[,i] <- colSums(A * w.knot)
-    lpost.grid[i] <- -(0.5*nknots+1.5)*log1p(0.5*r/1.5) - gridmats[nknots*(L+nknots)+1,i] + gridmats[nknots*(L+nknots)+2,i]
-  }
-  lpost.sum <- logsum(lpost.grid)
-  post.grid <- exp(lpost.grid - lpost.sum)
-  w <- c(w.grid %*% post.grid)
-  return(list(w = w, lpost.sum = lpost.sum))
-}
-
-ppFn <- function(w.knot, gridmats, L, nknots, ngrid, a.kap){
-  w.grid <- matrix(NA, L, ngrid)
-  lpost.grid <- rep(NA, ngrid)
-  for(i in 1:ngrid){
-    A <- matrix(gridmats[1:(L*nknots),i], nrow = nknots)
-    R <- matrix(gridmats[L*nknots + 1:(nknots*nknots),i], nrow = nknots)
-    r <- sum.sq(backsolve(R, w.knot, transpose = TRUE))
-    w.grid[,i] <- colSums(A * w.knot)
-    lpost.grid[i] <- (logsum(-(nknots/2+a.kap[1,])*log1p(0.5*r/ a.kap[2,]) + a.kap[3,] + lgamma(a.kap[1,]+nknots/2)-lgamma(a.kap[1,])-.5*nknots*log(a.kap[2,]))
-                      - gridmats[nknots*(L+nknots)+1,i] + gridmats[nknots*(L+nknots)+2,i])
-  }
-  lpost.sum <- logsum(lpost.grid)
-  post.grid <- exp(lpost.grid - lpost.sum)
-  w <- c(w.grid %*% post.grid)
-  return(list(w = w, lpost.sum = lpost.sum))
-}
-
-lamFn <- function(prox) return(sqrt(-100*log(prox)))
-nuFn <- function(z) return(0.5 + 5.5*exp(z/2))
-nuFn.inv <- function(nu) return(2*log((nu - 0.5)/5.5))
-sigFn <- function(z, a.sig) return(exp(z/2))
+lamFn     <- function(prox) return(sqrt(-100*log(prox)))
+nuFn      <- function(z) return(0.5 + 5.5*exp(z/2))
+nuFn.inv  <- function(nu) return(2*log((nu - 0.5)/5.5))
+sigFn     <- function(z, a.sig) return(exp(z/2))
 sigFn.inv <- function(s, a.sig) return(2 * log(s))
-unitFn <- function(u) return(pmin(1 - 1e-10, pmax(1e-10, u)))
-
-q0 <- function(u, nu = Inf) return(1 / (dt(qt(unitFn(u), df = nu), df = nu) * qt(.9, df = nu)))
-Q0 <- function(u, nu = Inf) return(qt(unitFn(u), df = nu) / qt(.9, df = nu))
-F0 <- function(x, nu = Inf) return(pt(x*qt(.9, df = nu), df = nu))
-
-sum.sq <- function(x) return(sum(x^2))
-extract <- function(lo, vn) return(lo[[vn]])
-logmean <- function(lx) return(max(lx) + log(mean(exp(lx - max(lx)))))
-logsum <- function(lx) return(logmean(lx) + log(length(lx)))
-shrinkFn <- function(x) return(1) ##(1/(1 + log(x)))
-trape <- function(x, h, len = length(x)) return(c(0, cumsum(.5 * (x[-1] + x[-len]) * (h[-1] - h[-len]))))
+unitFn    <- function(u) return(pmin(1 - 1e-10, pmax(1e-10, u)))
+logmean   <- function(lx) return(max(lx) + log(mean(exp(lx - max(lx)))))
+logsum    <- function(lx) return(logmean(lx) + log(length(lx)))
 
 klGP <- function(lam1, lam2, nknots = 11){
   tau <- seq(0, 1, len = nknots)
